88ab697474711c54be9de367eb6c82d8f0a1d552
Jeannie Chirayus <iamjeannie@gmail.com>
2014-08-05 19:50:50 -0400

    openshift

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,27 +24,27 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-// $env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(array(
+
+	// 'local' => array('homestead'),
+	// 'local' => array('*'),
+	'local' => array('Jeannie_Harvard'),
+  'production' => array('ex-std-node232.prod.rhcloud.com'),
+));
+
+// $env = $app->detectEnvironment(function() {
 //
-// 	// 'local' => array('homestead'),
-// 	// 'local' => array('*'),
-// 	'local' => array('Jeannie_Harvard'),
-//   'production' => array('ex-std-node292.prod.rhcloud.com'),
-// ));
-
-$env = $app->detectEnvironment(function() {
-
-    # See if there's an environment.php file...
-    # If it exists, get environment string from there
-    if(file_exists(__DIR__.'/../environment.php')) {
-        return require __DIR__.'/../environment.php';
-    }
-    # If not found, assume production
-    else {
-        return 'production';
-    }
-
-});
+//     # See if there's an environment.php file...
+//     # If it exists, get environment string from there
+//     if(file_exists(__DIR__.'/../environment.php')) {
+//         return require __DIR__.'/../environment.php';
+//     }
+//     # If not found, assume production
+//     else {
+//         return 'production';
+//     }
+//
+// });
 
 /*
 |--------------------------------------------------------------------------
