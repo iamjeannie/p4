855a70d1ee31fb3e46b70402f3fbb31f47eb179f
Jeannie Chirayus <iamjeannie@gmail.com>
2014-08-05 21:33:21 -0400

    production

---
+++ app/config/production/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => '127.3.186.130',
+			'database'  => 'authapp',
+			'username'  => 'admin8fAQtam',
+			'password'  => '6VSQH27PIp44',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+	),
+
+);
